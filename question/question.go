fca4eaf4b2110c025f429dd60744cfef7efef407
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-11 22:57:37 +0530

    create struct question

--- question/question.go
+++ question/question.go
@@ -1,5 +1,17 @@
 package question
 
-
-
-
+type Question struct {
+	ID      byte
+	QR      byte
+	OPCODE  byte
+	AA      byte
+	TC      byte
+	RD      byte
+	RA      byte
+	Z       byte
+	RCODE   byte
+	QDCOUNT byte
+	ANCOUNT byte
+	NSCOUNT byte
+	ARCOUNT byte
+}
