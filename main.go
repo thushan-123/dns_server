f5f9530cfb1481ae37bc018112e20b443801231e
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-11 16:39:15 +0530

    create udp socket

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
+
+	// memory buffer
+	p := make([]byte, 4048)
+
 	// create the udp socket listen port 53
+
+	addr := net.UDPAddr{
+		Port: 1234,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+
+	s, err := net.ListenUDP("udp", &addr)
+
+	if err != nil {
+		fmt.Print("error ", err)
+	}
+
+	for {
+		_, clientAdress, err := s.ReadFromUDP(p)
+
+		if err != nil {
+			fmt.Print("\nerror :", err)
+
+		}
+		s.WriteToUDP([]byte("hii"), clientAdress)
+	}
+
 }
