587d9fd7738248996532302dce7d8371aa4b57cb
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-15 11:06:49 +0530

    add constatnt dns types

--- config/config.go
+++ config/config.go
@@ -1,5 +1,17 @@
 package config
 
+// dns record type
+
+const (
+	TypeA     = 1
+	TypeNS    = 2
+	TypeCNAME = 5
+	TypeSOA   = 6
+	TypeMX    = 15
+	TypeTXT   = 16
+	TypeAAA   = 28
+)
+
 type DNSRecord struct {
 	Name  string
 	Type  uint16
@@ -7,7 +19,6 @@ type DNSRecord struct {
 	TTL   uint32
 	Data  string
 }
-
 
 // dns class
 const (
