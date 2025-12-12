c19d29b3811db1bacb1815952e09cfb9e9d2f3c2
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-12 06:23:11 +0530

    implement header section

--- header/header.go
+++ header/header.go
@@ -1,34 +1,6 @@
-package question
+package header
 
 import "encoding/binary"
-
-
-type DnsHeader struct {
-	ID      uint16 // alls 16 bits
-	Falgs   uint16 //  QR- 1 | OPCODE- 4 | AA-1 |  TC -1 |  RD- 1 |  RA -1 |  Z- 3  | RCODE- 4
-	RCODE   uint16
-	QDCOUNT uint16
-	ANCOUNT uint16
-	NSCOUNT uint16
-	ARCOUNT uint16
-}
-
-type DNSMessage []byte
-
-func CreateHeader(dnsHeader DnsHeader) DNSMessage {
-	header := make([]byte, 12)
-
-	binary.BigEndian.PutUint16(header[0:2], dnsHeader.ID)
-	binary.BigEndian.PutUint16(header[2:4], dnsHeader.Falgs)
-	binary.BigEndian.PutUint16(header[4:6], dnsHeader.QDCOUNT)
-	binary.BigEndian.PutUint16(header[6:8], dnsHeader.ANCOUNT)
-	binary.BigEndian.PutUint16(header[8:10], dnsHeader.NSCOUNT)
-	binary.BigEndian.PutUint16(header[10:12], dnsHeader.ARCOUNT)
-
-	return header
-}
-
-
 
 /*
 	ID - 16 bits
@@ -46,3 +18,32 @@ func CreateHeader(dnsHeader DnsHeader) DNSMessage {
 	ARCOUNT - 16 bit
 
 */
+
+type DNSHeader struct {
+	ID      uint16
+	Flags   uint16
+	QDCount uint16
+	ANCount uint16
+	NSCount uint16
+	ARCount uint16
+}
+
+type DNSMessage []byte
+
+func CreateHeader(h DNSHeader) DNSMessage {
+	header := make([]byte, 12)
+
+	binary.BigEndian.PutUint16(header[0:2], h.ID)
+	binary.BigEndian.PutUint16(header[2:4], h.Flags)
+	binary.BigEndian.PutUint16(header[4:6], h.QDCount)
+	binary.BigEndian.PutUint16(header[6:8], h.ANCount)
+	binary.BigEndian.PutUint16(header[8:10], h.NSCount)
+	binary.BigEndian.PutUint16(header[10:12], h.ARCount)
+
+	return header
+}
+
+
+
+
+
