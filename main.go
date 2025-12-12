ec2e125917cef9c464e4a19a0608e48906346462
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-12 06:30:52 +0530

    update main.go

--- main.go
+++ main.go
@@ -1,51 +1,108 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
+
+	"dns/encodeDecode"
+	"dns/header"
 )
 
-func SendMessage(connection net.Conn, clientAddress net.UDPAddr) {
+func parseDomain(msg []byte, offset int) (string, int) {
+	var domain string
+	for {
+		l := int(msg[offset])
+		if l == 0 {
+			offset++
+			break
+		}
+		offset++
+		domain += string(msg[offset:offset+l]) + "."
+		offset += l
+	}
+	return domain, offset
+}
 
+func buildARecord(queryID uint16, domain string, ip string) []byte {
+	// Header
+	respHeader := header.DNSHeader{
+		ID:      queryID,
+		Flags:   0x8180, // standard response, no error
+		QDCount: 1,
+		ANCount: 1,
+		NSCount: 0,
+		ARCount: 0,
+	}
+
+	msg := header.CreateHeader(respHeader)
+
+	// Question section (repeat same query)
+	msg = append(msg, encodeDecode.EncodeDomain(domain)...)
+	qtype := make([]byte, 4)
+	binary.BigEndian.PutUint16(qtype[0:2], 1) // type A
+	binary.BigEndian.PutUint16(qtype[2:4], 1) // class IN
+	msg = append(msg, qtype...)
+
+	// Answer section
+	msg = append(msg, encodeDecode.EncodeDomain(domain)...)
+
+	atype := make([]byte, 10)
+	binary.BigEndian.PutUint16(atype[0:2], 1)     // type A
+	binary.BigEndian.PutUint16(atype[2:4], 1)     // class IN
+	binary.BigEndian.PutUint32(atype[4:8], 60)    // TTL
+	binary.BigEndian.PutUint16(atype[8:10], 4)    // IPv4 length
+	msg = append(msg, atype...)
+
+	ipBytes := net.ParseIP(ip).To4()
+	msg = append(msg, ipBytes...)
+
+	return msg
 }
 
 func main() {
-
-	// memory buffer
-	p := make([]byte, 4048)
-
-	// create the udp socket listen port 53
+	buffer := make([]byte, 4096)
 
 	addr := net.UDPAddr{
 		Port: 1234,
 		IP:   net.ParseIP("127.0.0.1"),
 	}
 
-	connection, err := net.ListenUDP("udp", &addr)
-
+	conn, err := net.ListenUDP("udp", &addr)
 	if err != nil {
-		fmt.Print("error ", err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	defer connection.Close()
-
-	fmt.Println("Server Is Running on localhost port 1234")
+	fmt.Println("DNS Server running on 127.0.0.1:1234")
 
 	for {
-		i, clientAdress, err := connection.ReadFromUDP(p)
-
+		n, clientAddr, err := conn.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Print("\nerror :", err)
-
+			fmt.Println("Read error:", err)
+			continue
 		}
-		
-		fmt.Println(p[:i])
 
-		connection.WriteToUDP([]byte("hii"), clientAdress) // client respons befor send
+		msg := buffer[:n]
+
+		//header
+		id := binary.BigEndian.Uint16(msg[0:2])
+		offset := 12
+
+		//domain
+		domain, _ := parseDomain(msg, offset)
+		domain = domain[:len(domain)-1] 
+
+		fmt.Println("Query for:", domain)
+
+		//response
+		resp := buildARecord(id, domain, "127.0.0.1")
+
+		conn.WriteToUDP(resp, clientAddr)
 	}
-
 }
+
 
 // dig @127.0.0.1 -p 1234 test.com A
