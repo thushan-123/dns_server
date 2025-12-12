8ad5e116411ac516ce1635cc872cc80c980c5692
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-12 06:25:09 +0530

    encodse

--- encodeDecode/encode.go
+++ encodeDecode/encode.go
@@ -1,6 +1,17 @@
-package encodedecode
+package encodeDecode
 
+import (
+	"strings"
+)
 
-// func DecodeDnsName(data [] byte) (string, error){
+func EncodeDomain(domain string) []byte {
+	parts := strings.Split(domain, ".")
+	var encoded []byte
 
-// }
+	for _, p := range parts {
+		encoded = append(encoded, byte(len(p)))
+		encoded = append(encoded, []byte(p)...)
+	}
+
+	return append(encoded, 0)
+}
